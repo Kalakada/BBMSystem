f9edb75b8ae25ac780b270a0baab0f95af09d732
Kalakada <kalakadakarthik@gmail.com>
2022-07-01 11:01:48 +0530

    upload

--- connection2.php
+++ connection2.php
@@ -2,28 +2,27 @@
 <body>
 <?php 
  include_once("connection.php"); 
- session_start(); //always start a session in the beginning 
+ session_start(); 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 { 
-    if (empty($_POST['username']) || empty($_POST['password'])) //Validating inputs using PHP code 
+    if (empty($_POST['username']) || empty($_POST['password']))
     { 
         echo 
-        "Incorrect username or password"; //
-        header("location: login.php");//You will be sent to Login.php for re-login 
+        "Incorrect username or password"; 
+        header("location: login.php"); 
     } 
  
-     $inUsername = $_POST["username"]; // as the method type in the form is "post" we are using $_POST otherwise it would be $_GET[] 
+     $inUsername = $_POST["username"];
      $inPassword = $_POST["password"]; 
-     $stmt= $conn->prepare("SELECT USERNAME, PASSWORD FROM LOGIN WHERE USERNAME = '$inUsername' AND PASSWORD = '$inPassword'"); //Fetching all the records with input credentials
-     $stmt->bind_param("s",$inUsername); //bind_param() - Binds variables to a prepared statement as parameters. "s" indicates the type of the parameter.
+     $stmt= $conn->prepare("SELECT USERNAME, PASSWORD FROM LOGIN WHERE USERNAME = '$inUsername' AND PASSWORD = '$inPassword'"); 
+     $stmt->bind_param("s",$inUsername); 
      $stmt->execute();
-     $stmt->bind_result($UsernameDB, $PasswordDB); // Binding i.e. mapping database results to new variables
-      
-    //Compare if the database has username and password entered by the user. Password has to be decrypted while comparing.
+     $stmt->bind_result($UsernameDB, $PasswordDB); 
+
      if ($stmt->fetch() ) 
      {
-        $_SESSION['username']=$inUsername ; //Storing the username value in session variable so that it can be retrieved on other pages
-        header("location: Home.php"); // user will be taken to profile page
+        $_SESSION['username']=$inUsername ; 
+        header("location: Home.php"); 
      }
      else
      {
