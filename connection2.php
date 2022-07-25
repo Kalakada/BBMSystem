b2c3be3ddf0071e90b7431b7f2ac6c415598dbd6
Kalakada <kalakadakarthik@gmail.com>
2022-07-25 10:05:41 +0530

    upload

--- connection2.php
+++ connection2.php
@@ -5,20 +5,18 @@
  session_start(); 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 { 
-    if (empty($_POST['username']) || empty($_POST['password']))
+    if (empty($_POST['username']) || empty($_POST['password']))  
     { 
         echo 
         "Incorrect username or password"; 
         header("location: login.php"); 
-    } 
- 
-     $inUsername = $_POST["username"];
+    }
+     $inUsername = $_POST["username"];  
      $inPassword = $_POST["password"]; 
      $stmt= $conn->prepare("SELECT USERNAME, PASSWORD FROM LOGIN WHERE USERNAME = '$inUsername' AND PASSWORD = '$inPassword'"); 
      $stmt->bind_param("s",$inUsername); 
      $stmt->execute();
      $stmt->bind_result($UsernameDB, $PasswordDB); 
-
      if ($stmt->fetch() ) 
      {
         $_SESSION['username']=$inUsername ; 
